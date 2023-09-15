45458aae605671591acafa27d2ddf1cb7778e8c7
NicolasLaure <nicolasariellaure@gmail.com>
2023-09-15 10:10:00 -0300

    Add basic Colors into Color.h

--- BreakOut-Sigil/src/GameManagement/Color.h
+++ BreakOut-Sigil/src/GameManagement/Color.h
@@ -10,9 +10,12 @@ struct Color
 namespace colors
 {
 	Color WHITE = { 252,252,252 };
-	Color LIGHT_GRAY = {};
-	Color GRAY = {};
-	Color DARK_GRAY = {};
+	Color LIGHT_GRAY = { 196,196,196 };
+	Color GRAY = { 125,125,125 };
+	Color DARK_GRAY = { 69,69,69 };
 	Color BLACK = { 15,15,15 };
 	Color RED = { 230,11,11 };
+	Color GREEN = { 27, 194, 33 };
+	Color BLUE = { 23, 51, 232 };
+	Color YELLOW = { 235, 213, 16 };
 };
